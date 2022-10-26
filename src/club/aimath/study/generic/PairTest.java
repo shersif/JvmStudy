79506e8d68c7361fb4f1b77ad03f7ce634834b30
outx <outingxiao@126.com>
2022-10-26 23:29:25 +0800

    8.6.5

--- src/club/aimath/study/generic/PairTest.java
+++ src/club/aimath/study/generic/PairTest.java
@@ -1,8 +1,9 @@
 package club.aimath.study.generic;
 
-import jdk.jfr.Label;
-
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
 
 /**
  * @Description 泛型测试
@@ -21,9 +22,28 @@ public class PairTest{
 //        System.out.println("min = " + min);
 
         DateInterval dateInterval = new DateInterval();
-//        Pair<LocalDate> pair=dateInterval;
-        dateInterval.setSecond(LocalDate.now());
-        dateInterval.setSecond("Heele");
+        Pair<LocalDate> pair=dateInterval;
+        pair.setSecond(LocalDate.now());
+        System.out.println("pair instanceof Pair = " + (pair instanceof Pair));
+
+        Collection<Pair<String>> collection=new HashSet<>();
+        Pair<String> pair1=new Pair<>("ou","ting");
+        StringInterval pair2=new StringInterval("ting","xiao");
+        Object[] pairs = addAll(collection, pair1, pair2);
+        collection.stream().forEach(System.out::println);
+        pairs[0]=new Pair<>(12,18);// 成功,符合擦除类型:Pair<?>
+//      pairs[0]= "nihao"; 失败,不符合Pair<?> 类型 报:ArrayStoreException错误
+        for (int i = 0; i < pairs.length; i++) {
+            System.out.println("pairs["+i+"] = " + pairs[i]);
+        }
+    }
+
+    public static <T> T[] addAll(Collection<T> collection,T... ts){
+        for (T t:ts
+             ) {
+            collection.add(t);
+        }
+        return ts;
     }
 }
 
@@ -78,21 +98,31 @@ class Pair<T>{
         System.out.println("fater received second = " + second);
         this.second = second;
     }
-}
-
-class DateInterval extends Pair{
 
     @Override
-    public void setSecond(Object second) {
-        super.setSecond(second);
-        System.out.println("origin second = " + second);
+    public String toString() {
+        return getClass().getSimpleName()+"[" +
+                "first=" + first +
+                ", second=" + second +
+                ']';
     }
+}
+
+class DateInterval extends Pair<LocalDate>{
 
     public void setSecond(LocalDate second){
+//        boolean b = this instanceof Pair;
+//        System.out.println("this instanceof Pair:"+b);
         System.out.println("LocalDate second = " + second);
+        StackWalker walker=StackWalker.getInstance();
+        walker.forEach(stackFrame->{
+            System.out.println(stackFrame);
+        });
     }
+}
 
-    public LocalDate getSecond(){
-        return (LocalDate) super.getSecond();
+class StringInterval extends Pair<String>{
+    public StringInterval(String first, String second) {
+        super(first, second);
     }
 }
