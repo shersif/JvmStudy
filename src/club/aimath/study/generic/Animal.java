5668cfc647848123f2d50e6aa93c04e3193f4968
outx <outingxiao@126.com>
2022-11-03 00:49:58 +0800

    8.9.2

--- src/club/aimath/study/generic/Animal.java
+++ src/club/aimath/study/generic/Animal.java
@@ -16,6 +16,22 @@ public class Animal {
      */
     private String color;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
     public Animal(String name, String color) {
         this.name = name;
         this.color = color;
